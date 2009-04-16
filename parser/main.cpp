f8e3b387536b428e66e45961946add10b126d3d5
aljosa.osep <aljosa.osep@07432978-1335-11de-a4db-e31d5fa7c4f0>
2009-04-16 18:14:20 +0000

    Dodan razred za parsanje vzorcev

---
+++ parser/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "nnetparser.h"
+
+using namespace std;
+
+
+CNNParser parser("test.txt");
+
+//string str = "1.5 3433435 2 4";
+string token;
+int pos;
+int main()
+{
+    parser.parse();
+    parser.getPatterns().print();
+
+    return 0;
+}
