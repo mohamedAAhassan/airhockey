c3501b2fd24cb9f5d06deeae2b1971d18f05673e
mitjasantl <mitjasantl@07432978-1335-11de-a4db-e31d5fa7c4f0>
2009-04-17 09:03:59 +0000

    dodan simulator

---
+++ trunk/simulator/AHokey/ChildView.h
@@ -0,0 +1,79 @@
+// ChildView.h : interface of the CChildView class
+//
+
+
+#pragma once
+#include "afxwin.h"
+#include "MemDC.h"
+#include "globals.h"
+#include <math.h>
+
+
+// CChildView window
+
+class CChildView : public CWnd
+{
+// Construction
+public:
+	CChildView();
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	protected:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+// Implementation
+public:
+	virtual ~CChildView();
+
+	// Generated message map functions
+protected:
+	afx_msg void OnPaint();
+	DECLARE_MESSAGE_MAP()
+public:
+	MALET pl1Malet;   // igralna plošèica prvega igralca
+	MALET pl2Malet;   // igralna plošèica drugega igralca
+	PUCK puck;        // pak
+	bool runGame;     // indikator, èe se je igra zaèela
+	int old_mal1_x;   // pomožne spremenljivke za raèunanje nove pozicije igralèeve plošèice
+	int old_mal1_y;
+	CPen *tableBorPen;   // Karakteristika èrte za obrobo mize
+	CPen* midLinePen;    // Karakteristika sredinske ètre
+	CPen *tableBlLine;   // Karakteristika modre èrte
+	CPen *redLinePen;    // Karakteristika èrte za rdeèe oznake na mizi
+	CFont timeFont;      // Font za izpis èasa igre
+	CFont scoreFont;     // Font za izpis rezultata
+	CBrush tableBrush;   // Karakteristika ozadja mize
+	MYTIME playTime;     // Èas, ki ga imamo na voljo za konèanje igre
+	int pl1Score;        // Toèke igralca 1
+	int pl2Score;        // Toèke igralca 2
+	int startTick;		 // zaèeten èas, ko se igra zaène
+
+
+	afx_msg void OnShowWindow(BOOL bShow, UINT nStatus);
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	afx_msg void OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	void PaintScreen(CMemDC * dc);
+	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
+	// Izracun dolzine vektorja
+	double vcLength(double cX, double cY);
+	void generateNewPuckDirection(void);
+	void pl2premikmalet(void);
+	bool odbojstene(void);
+	bool odbojmaleta(void);
+	void EstimateNewPuckPossition(void);
+	bool validPuckPos(void);
+	void calcTime(void);
+	void DetermineGoal();
+	bool isLeftGoal();
+	bool isRightGoal();
+	// Preverimo, èe lahko gre pak v gol
+	bool PossibleGoal(void);
+};
+
---
+++ trunk/simulator/AHokey/globals.h
@@ -0,0 +1,38 @@
+#ifndef globalsH
+#define globalsH
+
+#define MALET_RADIUS 20
+#define PUCK_RADIUS 15
+
+#define LEFT_UP_CORNER_X 30
+#define LEFT_UP_CORNER_Y 30
+#define RIGHT_D_CORNER_X 840
+#define RIGHT_D_CORNER_Y 430
+
+#define GOAL_UPPER_Y 184
+#define GOAL_LOWER_Y 284
+
+typedef struct stMALET{
+	int posX,posY;        // Položaj valja na igralni mizi
+	int radius;			  // Radij igralèevega valja
+	CPen *mPen;
+	CBrush mBrush;
+}MALET;
+
+typedef struct stPUCK{
+	int posX, posY;      // Polozaj paka na mizi
+	double dirX, dirY;   // Vektor smeri
+	double velocity;     // Hitrost premika paka
+	double par;          // Parameter premika
+	int radius;			 // radij paka
+	CPen *pPen;
+	CBrush pBrush;
+}PUCK;
+
+typedef struct stTIME{
+	int min;
+	int sec;
+}MYTIME;
+
+
+#endif globalsH
